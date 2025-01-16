d650dfa9034adbce00067e48db2c934f28336c19
BFerris <ferris.163@buckeyemail.osu.edu>
2025-01-16 14:54:38 -0500

    Added Links to Instagram and LinkedIn profiles

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -30,7 +30,19 @@ export function Header({
   ));
 
   return (
-    <Group justify="space-between" py={{ base: "sm", md: "md" }}>
+    <Group
+      justify="space-between"
+      py={{ base: "sm", md: "md" }}
+      style={{
+        backgroundColor: "#000", // Add solid background color
+        position: "fixed", // Fix the header at the top
+        top: 0,
+        left: 0,
+        width: "100%", // Ensure it spans the full width
+        zIndex: 1000, // Ensure it stays on top of other content
+        overflow: "hidden", // Hide scroll bars
+      }}
+    >
       <Button
         unstyled
         className={classes.button}
