4fe4bb1c986f10b6d4944b5a6ea81de4847c4b86
BFerris <ferris.163@buckeyemail.osu.edu>
2025-01-27 13:40:24 -0500

    Thumbnail fix #2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,7 +22,7 @@ export default function App({ Component, pageProps }: any) {
         <link rel="shortcut icon" href="/favicon.svg"></link>
         <meta property="og:title" content="Gimage" />
         <meta property="og:description" content="A fitness focused social media app to promote an active lifestyle, exchange ideas, and stay accountable." />
-        <meta property="og:image" content="/assets/LogoOrange.svg" />
+        <meta property="og:image" content="https://www.gimageco.com/assets/LogoOrange.svg" />
         <meta property="og:url" content="https://gimageco.com" />
         <meta property="og:type" content="website" />
       </Head>
